c7b80af3b14d9149c5628682c0771772adfa61c3
N Chandra Kiran Reddy <151184574+ChANDUREDDY49830@users.noreply.github.com>
2025-03-24 12:47:17 +0530

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,15 @@
+class StudentInfo {
+    public static void main(String[] args) {
+        // Define student details
+        String name = "N chandra kiran reddy";
+        int rollNo = 748304;
+        String branch = "Computer Science";
+
+        // Display student details
+        System.out.println("Student Details:");
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Branch: " + branch);
+    }
+}
+
