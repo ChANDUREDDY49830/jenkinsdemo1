f319858b653b7c17cca2795afec86906ad47ef15
N Chandra Kiran Reddy <151184574+ChANDUREDDY49830@users.noreply.github.com>
2025-03-25 12:03:38 +0530

    Add files via upload

---
+++ Appletdemo1.java
@@ -0,0 +1,12 @@
+import.java.applet.*;
+import.java.aws.*;
+
+/*<applet code="AppletDemo.class" width="300" height="300">
+</applet>
+public class AppletDemo extends Applet
+{
+public void pant(Graphics g)
+{
+g.drawString("Hello World!,100,150);
+}
+}
